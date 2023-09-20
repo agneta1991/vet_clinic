f800e68cff974998c3bcc8dfc30844e8fc8184dc
agneta1991 <pupienytea@gmail.com>
2023-09-20 12:45:47 +0300

    updating & using join

--- sql_files/schema.sql
+++ sql_files/schema.sql
@@ -15,3 +15,33 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD species varchar(100);
+
+
+CREATE TABLE owners (
+    id INT not null,
+    full_name varchar(100) not null,
+    age int not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id INT not null,
+    name varchar (100) not null,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
+
