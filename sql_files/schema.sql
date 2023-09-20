5293f847ca9af470c31a2242d9a50de65ee55c35
agneta1991 <pupienytea@gmail.com>
2023-09-20 19:03:15 +0300

    added data to tables, wrote queries commands

--- sql_files/schema.sql
+++ sql_files/schema.sql
@@ -47,3 +47,31 @@ ADD CONSTRAINT fk_owner
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
 
+-- JOIN TABLE VETS 
+
+CREATE TABLE vets (
+    id INT,
+    name varchar(100),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE specializations (
+    specialization_id INT,
+    species_id INT,
+    vet_id INT,
+    PRIMARY KEY(specialization_id),
+    FOREIGN KEY (species_id) REFERENCES species(id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    visit_id INT,
+    animal_id INT,
+    vet_id INT,
+    visit_date DATE,
+    PRIMARY KEY (visit_id),
+    FOREIGN KEY (animal_id) REFERENCES animals(id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
